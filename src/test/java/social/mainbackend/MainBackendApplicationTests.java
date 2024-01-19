2ccede06e59655650fdfeddfe155b7ecfdd504d1
Sanzhar Bekbolat <sanzharbekbolat1@gmail.com>
2024-01-20 01:07:19 +0600

    Initial commit

---
+++ src/test/java/social/mainbackend/MainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package social.mainbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
